fa35b4a3f71af046ae514c7b123c9dd54057ba6c
Jordan <Jordan@DESKTOP-1MQVID4>
2019-01-08 10:12:16 +0000

    Objetivo 2.1

--- src/examen/Pruebas.java
+++ src/examen/Pruebas.java
@@ -1,10 +1,18 @@
 package examen;
 
+import datos.Examen;
+
 public class Pruebas {
 
 	// Desarrolllar los métodos del examen AQUI (ver Objetivos)
 	public static void main(String[] args) {
 		// Hacer las llamadas a los métodos AQUI.
 		// (no olvidar recoger los valores devueltos
+		
+		// 2.1 declara e inicializa  dos objetos de la clase Examen.
+		Examen exm1 = new Examen();
+		Examen exm2 = new Examen();
+		
+		
 	}
 }
